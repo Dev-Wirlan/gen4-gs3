28a24449211f4e59a4d0ae69988431a322bde3a4
Dev-Wirlan <wirlansivieri04@gmail.com>
2026-09-24 09:57:16 -0300

    fix: restore spatial catalog metadata

--- src/converter/spatial-catalog-builder.ts
+++ src/converter/spatial-catalog-builder.ts
@@ -57,7 +57,7 @@ export function buildSpatialCatalog(
     <bt:UnitOfMeasureVersion major="1" minor="43" />
     <bt:RepresentationSystemVersion major="4" minor="161" />
   </FileSchemaVersion>
-  <SourceApp major="2.0" build="0" revision="135" nameSourceApp="RCD Target Provider" uuidSourceApp="{b050528e-f328-4dd8-9e9c-71fe8153692e}" uuidSourceAppNode="{${node}}" uuidSession="{${makeUuid()}}" />
+  <SourceApp major="2" minor="0" build="0" revision="135" nameSourceApp="RCD Target Provider" uuidSourceApp="{b050528e-f328-4dd8-9e9c-71fe8153692e}" uuidSourceAppNode="{${node}}" uuidSession="{${makeUuid()}}" />
   <Setup>
     <rcdsetup:FileSchemaVersion nonProductionCode="0"><bt:FileSchemaContentVersion major="3" minor="28" /><bt:UnitOfMeasureVersion major="1" minor="43" /><bt:RepresentationSystemVersion major="4" minor="161" /></rcdsetup:FileSchemaVersion>
     <bt:Synchronization><bt:NodeVersions><bt:Node uuid="{${node}}" lastSeen="${now}" /></bt:NodeVersions><bt:EntityDeletions /></bt:Synchronization>
